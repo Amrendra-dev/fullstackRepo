c149c5f09d461e0ddc2c056bcc3eb111936b5429
hp <hp@Amrendra>
2025-08-09 17:22:28 +0530

    Dev1 can dev1

--- Proj01/src/com/am/tl/Dev1.java
+++ Proj01/src/com/am/tl/Dev1.java
@@ -3,7 +3,7 @@ package com.am.tl;
 public class Dev1 {
 
 	public static void main(String[] args) {
-		System.out.println("Dev 1 Add Te class");
+		//System.out.println("Dev 1 Add Te class");
 			}
 
 }
